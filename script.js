0a480bd3e8ac457741eafe91d45022bc5162549f
Michael Epps <meppscomplex@gmail.com>
2019-10-18 12:47:06 -0700

    Added

---
+++ script.js
@@ -0,0 +1,31 @@
+    player = 'x' || 'y';
+
+
+   var cell1 = document.getElementById('cell1');
+   var cell2 = document.getElementById('cell2');
+   var cell3 = document.getElementById('cell3');
+   var cell4 = document.getElementById('cell4');
+   var cell5 = document.getElementById('cell5');
+   var cell6 = document.getElementById('cell6');
+   var cell7 = document.getElementById('cell7');
+   var cell8 = document.getElementById('cell8');
+   var cell9 = document.getElementById('cell9');
+
+   function runGame(){
+
+       cell1.addEventListener('click', takeTurn);
+       cell2.addEventListener('click', takeTurn);
+       cell3.addEventListener('click', takeTurn);
+       cell4.addEventListener('click', takeTurn);
+       cell5.addEventListener('click', takeTurn);
+       cell6.addEventListener('click', takeTurn);
+       cell7.addEventListener('click', takeTurn);
+       cell8.addEventListener('click', takeTurn);
+       cell9.addEventListener('click', takeTurn);
+
+    function takeTurn(e){
+        target.innerHTML = 'X';
+    }
+   }
+
+   runGame()
